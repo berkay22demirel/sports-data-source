2ac7d501485f4940af8e60d416349b9e0caf7961
berkay-demirel <berkay_demirel22@hotmail.com>
2021-02-21 16:11:31 +0300

    Initial commit

---
+++ src/main/java/com/berkay22demirel/sportsdatasource/SportsDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.sportsdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsDataSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkay22demirel/sportsdatasource/SportsDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.sportsdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsDataSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
