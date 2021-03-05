ec61c69577f028aa873cc0e14506aab30e117bc0
berkay-demirel <berkay_demirel22@hotmail.com>
2021-03-05 12:07:13 +0300

    fix

--- src/main/java/com/berkay22demirel/sportsdatasource/SportsDataSourceApplication.java
+++ src/main/java/com/berkay22demirel/sportsdatasource/SportsDataSourceApplication.java
@@ -2,9 +2,10 @@ package com.berkay22demirel.sportsdatasource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SportsDataSourceApplication {
+public class SportsDataSourceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportsDataSourceApplication.class, args);
